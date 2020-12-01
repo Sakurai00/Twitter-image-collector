b193ae6cb3637e914a21ddcdfd7e4f1edeb29082
Sakurai <K.Sakurai_2000@outlook.com>
2020-12-01 16:00:26 +0900

    fix line

--- settings.py
+++ settings.py
@@ -8,18 +8,21 @@ ACCESS_TOKEN_KEY = os.environ.get("TW_ACC_TOKEN")
 ACCESS_TOKEN_SECRET = os.environ.get("TW_ACC_TOKEN_SEC")
 
 
-NO_INPUT = True
-MULTI_SEARCH = True
 
 SEARCH_NUM = 1000
 MIN_FAV = 50
+
+NO_INPUT = True
 TARGET_WORD = "#シオンの書物"
+
+MULTI_SEARCH = True
 MULTI_TARGET_WORD = [
     "#プロテインザスバル",
     "#シオンの書物",
     "#百鬼絵巻",
     "#みおーん絵"
     ]
+
 
 """
 #プロテインザスバル
