c5ca4e82c30658b0d00851d209a1036ac61d1bfa
Sakurai <K.Sakurai_2000@outlook.com>
2021-08-25 20:36:33 +0900

    add poetry

--- run.bat
+++ run.bat
@@ -1,3 +1,3 @@
 @echo off
-python -u "E:\Users\Sakurai\OneDrive\Code\Project\twitter-image-collector\collector.py"
+poetry run python -u "E:\Users\Sakurai\OneDrive\Code\Project\twitter-image-collector\collector.py"
 pause
